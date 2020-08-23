6c33501e7c8006989806ccc6e8e7a0006a538666
susilo hartomo <encusilo545110@gmail.com>
2020-08-23 17:00:47 +0700

    focused tab done

--- src/router/TopBarNavigation.js
+++ src/router/TopBarNavigation.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { View, Text, StyleSheet } from 'react-native';
 
 // screens
-import HomeScreen from '../screens/HomeScreen'
 import SettingsScreen from '../screens/SettingsScreen'
 import ProfileScreen from '../screens/ProfileScreen'
-import { View, Text } from 'react-native';
 
 export default function TopBarNavigation() {
     const Tab = createMaterialTopTabNavigator();
@@ -18,46 +17,115 @@ export default function TopBarNavigation() {
                 labelStyle: { fontSize: 12, },
                 scrollEnabled: true,
                 indicatorContainerStyle: { display: 'none' },
-                tabStyle: { width: 100 }
+                tabStyle: { width: 110, height: 60 }
             }}
         >
             <Tab.Screen
                 name="Profile"
                 component={ProfileScreen}
                 options={{
-                    tabBarLabel: 'Profile',
-                    tabBarIcon: ({ focused, color }) => {
-                        focused && <View style={{ backgroundColor: '#b1b1b1' }}>
-                            <Text style={{ color }}>OK</Text>
+                    tabBarLabel: ({ color, focused }) => (
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={styles[focused ? 'activeTopTab' : 'notActiveTabTob']}>
+                                <Text style={styles[focused ? 'activeText' : 'nonActiveText']}>Fashion</Text>
+                            </View>
                         </View>
-                    }
+                    ),
                 }}
             />
             <Tab.Screen
                 name="Notifications"
                 component={SettingsScreen}
-                options={{ tabBarLabel: 'Updates', }}
+                options={{
+                    tabBarLabel: ({ color, focused }) => (
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={styles[focused ? 'activeTopTab' : 'notActiveTabTob']}>
+                                <Text style={styles[focused ? 'activeText' : 'nonActiveText']}>Livestyle</Text>
+                            </View>
+                        </View>
+                    ),
+                }}
             />
             <Tab.Screen
                 name="Profile1"
                 component={ProfileScreen}
-                options={{ tabBarLabel: 'Profile' }}
+                options={{
+                    tabBarLabel: ({ color, focused }) => (
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={styles[focused ? 'activeTopTab' : 'notActiveTabTob']}>
+                                <Text style={styles[focused ? 'activeText' : 'nonActiveText']}>Hijub</Text>
+                            </View>
+                        </View>
+                    ),
+                }}
             />
             <Tab.Screen
                 name="Profile2"
                 component={ProfileScreen}
-                options={{ tabBarLabel: 'Profile' }}
+                options={{
+                    tabBarLabel: ({ color, focused }) => (
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={styles[focused ? 'activeTopTab' : 'notActiveTabTob']}>
+                                <Text style={styles[focused ? 'activeText' : 'nonActiveText']}>Cosmetics</Text>
+                            </View>
+                        </View>
+                    ),
+                }}
             />
             <Tab.Screen
                 name="Profile3"
                 component={ProfileScreen}
-                options={{ tabBarLabel: 'Profile' }}
+                options={{
+                    tabBarLabel: ({ color, focused }) => (
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={styles[focused ? 'activeTopTab' : 'notActiveTabTob']}>
+                                <Text style={styles[focused ? 'activeText' : 'nonActiveText']}>Fashion</Text>
+                            </View>
+                        </View>
+                    ),
+                }}
             />
             <Tab.Screen
                 name="Profile4"
                 component={ProfileScreen}
-                options={{ tabBarLabel: 'Profile' }}
+                options={{
+                    tabBarLabel: ({ color, focused }) => (
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={styles[focused ? 'activeTopTab' : 'notActiveTabTob']}>
+                                <Text style={styles[focused ? 'activeText' : 'nonActiveText']}>Healty</Text>
+                            </View>
+                        </View>
+                    ),
+                }}
             />
         </Tab.Navigator>
     );
 }
+
+const styles = StyleSheet.create({
+    activeTopTab: {
+        backgroundColor: '#f1f1f1',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 6,
+        paddingHorizontal: 16,
+        borderRadius: 40,
+        height: 40,
+        width: 110,
+        top: -6
+    },
+    notActiveTopTab: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 8,
+        paddingHorizontal: 16,
+        height: 40,
+        width: 80,
+    },
+    activeText: {
+        color: 'red'
+    },
+    nonActiveText: {
+        color: '#b4b4b4',
+    }
+})
